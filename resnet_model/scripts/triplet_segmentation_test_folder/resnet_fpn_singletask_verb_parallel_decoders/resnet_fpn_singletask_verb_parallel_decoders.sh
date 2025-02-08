caffb1ebb3297cb4feaa58d44c3a6d4c5b2072f5
Tosin Alabi <labdeeman77@gmail.com>
2025-02-08 16:44:11 +0000

    single class predition wit parallel decoders

---
+++ resnet_model/scripts/triplet_segmentation_test_folder/resnet_fpn_singletask_verb_parallel_decoders/resnet_fpn_singletask_verb_parallel_decoders.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+echo "started ..."
+
+cd /nfs/home/talabi/repositories/triplet_segmentation/resnet_model
+
+echo "train and test resnet fpn single task verb parrallel decoders..."
+
+python main.py --config config.triplet_segmentation_test_folder.singletask_resnet_fpn_verb_parallel_decoders
+
+echo "completed train and test resnet fpn single task verb parallel decoders..."
