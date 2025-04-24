6221874c7b0b841e90ade14e77e5d5a9e396d8fa
Tosin Alabi <tal22@isd.kcl.ac.uk>
2025-04-24 17:46:02 +0100

    resnet model v2_5

--- resnet_model/config/triplet_segmentation_v2_5_dataset/singletask_resnet_fpn_verbtarget_v2_5.py
+++ resnet_model/config/triplet_segmentation_v2_5_dataset/singletask_resnet_fpn_verbtarget_v2_5.py
@@ -2,8 +2,12 @@ from os.path import join
 import os
 
 # Hyperparameters
-batch_size = 64
-num_epochs = 20
+# batch_size = 64
+# num_epochs = 20
+# learning_rate = 0.0005
+
+batch_size = 1
+num_epochs = 1
 learning_rate = 0.0005
 
 # Dynamically set the experiment name from the filename
@@ -14,12 +18,14 @@ experiment_name = os.path.splitext(os.path.basename(__file__))[0]
 model_name = 'SingleTaskResNetFPN'
 task_name = 'verbtarget'
 description =  f'resnet_fpn predicting {task_name} only'
+architecture = 'singletask'
 
 # Dataset Directories
+
 if os.name == 'posix':  # Unix-like systems (Linux, macOS)
-    dataset_path = '/nfs/home/talabi/data/triplet_segmentation_dataset_v2_second_stage'
+    dataset_path = '/nfs/home/talabi/data/triplet_segmentation_dataset_v2_5_second_stage_solved_matching'
 elif os.name == 'nt':  # Windows systems
-    dataset_path = 'C:/Users/tal22/Documents/repositories/triplet_segmentation/data/triplet_segmentation_dataset_v2_second_stage'    
+    dataset_path = 'C:/Users/tal22/Documents/repositories/datasets/my_triplet_seg_datasets/triplet_segmentation_dataset_v2_5_second_stage_solved_matching'    
 else:
     raise EnvironmentError("Unsupported operating system. Unable to set dataset_path.")
 
@@ -37,12 +43,15 @@ test_ann_dir = join(dataset_path, 'mask2former_instrument_prediction/ann_second_
 # Working Directory
 work_dir = f'../resnet_model/work_dirs/{experiment_name}'
 save_results_path = join(work_dir, 'results.json')
-save_latest_results_path = join(work_dir, 'results_latest.json')
+save_logits_path = join(work_dir, 'results_logits.json')
+vis_dir =  join(work_dir, 'vis_dir/')
 
 # Checkpoint and Prediction Settings
 allow_resume = True # allows resumption from latest checkpoint
 load_from_checkpoint = None
 predict_only_mode = False
+use_wce = False
 
 # Other Constants
-image_size = (224, 224)
+image_size = (256, 448)
+model_input_size = (256, 448)
